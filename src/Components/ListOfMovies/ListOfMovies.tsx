a9a0ab0b4eb6d485afd396d30f81b11ec847e957
karolarizag <karolarizadev@gmail.com>
2023-09-24 22:18:57 +0100

    details in movies cards

--- src/Components/ListOfMovies/ListOfMovies.tsx
+++ src/Components/ListOfMovies/ListOfMovies.tsx
@@ -1,13 +1,14 @@
-import { GetMoviesByGenre } from "../../Services/GetMoviesByGenre"
-import { Rail } from "../Rail"
+import { GetMoviesByGenre } from "../../Services/GetMoviesByGenre";
+import { Rail } from "../Rail";
+import { GenreTitleStyled } from "./styled";
 
-export const ListOfMovies = ({id, name}: {id: number, name: string}) => {
-  const { genreMovies } = GetMoviesByGenre(id)
+export const ListOfMovies = ({ id, name }: { id: number; name: string }) => {
+  const { genreMovies } = GetMoviesByGenre(id);
 
   return (
     <>
-      <div >{name}</div>
+      <GenreTitleStyled>{name}</GenreTitleStyled>
       <Rail movies={genreMovies} type="horizontal-cards" />
     </>
-  )
-}
+  );
+};
